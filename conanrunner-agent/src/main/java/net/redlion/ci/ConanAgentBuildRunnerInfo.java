27bf57e14a32ddc0ed663bfa63eecb66606b35d6
David Zemon <david.zemon@redlion.net>
2019-10-23 14:05:02 -0500

    Add some safety to prevent runs when they wouldn't be possible anyway

--- conanrunner-agent/src/main/java/net/redlion/ci/ConanAgentBuildRunnerInfo.java
+++ conanrunner-agent/src/main/java/net/redlion/ci/ConanAgentBuildRunnerInfo.java
@@ -2,7 +2,16 @@ package net.redlion.ci;
 
 import jetbrains.buildServer.agent.AgentBuildRunnerInfo;
 import jetbrains.buildServer.agent.BuildAgentConfiguration;
+import jetbrains.buildServer.util.FileUtil;
+import jetbrains.buildServer.util.StringUtil;
 import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class ConanAgentBuildRunnerInfo implements AgentBuildRunnerInfo {
     @NotNull
@@ -11,6 +20,26 @@ public class ConanAgentBuildRunnerInfo implements AgentBuildRunnerInfo {
     }
 
     public boolean canRun(@NotNull final BuildAgentConfiguration agentConfiguration) {
-        return true; // TODO: Run some basic checks
+        boolean canRun;
+        final Map<String, String> configParams = agentConfiguration.getConfigurationParameters();
+        if (!StringUtil.isEmptyOrSpaces(configParams.getOrDefault(ConanConstants.CONAN_DOCKER_IMAGE_NAME_KEY, null))) {
+            canRun = this.executableIsValid("docker");
+        } else if (!StringUtil.isEmptyOrSpaces(configParams.getOrDefault(ConanConstants.CONAN_COMMAND_KEY, null))) {
+            canRun = this.executableIsValid(configParams.get(ConanConstants.CONAN_COMMAND_KEY).trim());
+        } else {
+            canRun = this.executableIsValid("conan");
+        }
+        return canRun;
+    }
+
+    public boolean executableIsValid(@NotNull final String executablePath) {
+        final File executable = new File(executablePath);
+        if (FileUtil.isAbsolute(executablePath)) {
+            return executable.canExecute();
+        } else {
+            return Stream.of(System.getenv("PATH").split(Pattern.quote(File.pathSeparator)))
+                    .map(Paths::get)
+                    .anyMatch(path -> Files.isExecutable(path.resolve(executable.toPath())));
+        }
     }
 }
--- conanrunner-server/src/main/java/net/redlion/ci/ConanRunnerRunType.java
+++ conanrunner-server/src/main/java/net/redlion/ci/ConanRunnerRunType.java
@@ -1,12 +1,17 @@
 package net.redlion.ci;
 
+import jetbrains.buildServer.requirements.Requirement;
+import jetbrains.buildServer.requirements.RequirementType;
 import jetbrains.buildServer.serverSide.PropertiesProcessor;
 import jetbrains.buildServer.serverSide.RunType;
 import jetbrains.buildServer.serverSide.RunTypeRegistry;
+import jetbrains.buildServer.util.StringUtil;
 import jetbrains.buildServer.web.openapi.PluginDescriptor;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class ConanRunnerRunType extends RunType {
@@ -64,5 +69,15 @@ public class ConanRunnerRunType extends RunType {
     @Override
     public Map<String, String> getDefaultRunnerProperties() {
         return null;
+    }
+
+    @NotNull
+    @Override
+    public List<Requirement> getRunnerSpecificRequirements(@NotNull Map<String, String> runParameters) {
+        final List<Requirement> requirements = new ArrayList<>();
+        if (!StringUtil.isEmptyOrSpaces(runParameters.getOrDefault(ConanConstants.CONAN_DOCKER_IMAGE_NAME_KEY, null))) {
+            requirements.add(new Requirement("docker.server.version", null, RequirementType.EXISTS));
+        }
+        return requirements;
     }
 }
