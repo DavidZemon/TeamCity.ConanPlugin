498ea4168a503e96a1bb29ec0e592dd4795a1ba0
David Zemon <david.zemon@redlion.net>
2019-10-23 14:17:57 -0500

    Ensure required parameters are actually required

--- conanrunner-server/src/main/java/net/redlion/ci/ConanPropertiesProcessor.java
+++ conanrunner-server/src/main/java/net/redlion/ci/ConanPropertiesProcessor.java
@@ -5,15 +5,20 @@ import jetbrains.buildServer.serverSide.PropertiesProcessor;
 import jetbrains.buildServer.util.StringUtil;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Map;
+import java.util.*;
 
 public class ConanPropertiesProcessor implements PropertiesProcessor {
     @Override
     public Collection<InvalidProperty> process(@NotNull final Map<String, String> properties) {
+        final List<InvalidProperty> invalidParams = new ArrayList<>();
         this.removeEmptyParameters(properties);
-        return Collections.emptyList();
+        if (StringUtil.isEmptyOrSpaces(properties.getOrDefault(ConanConstants.CONAN_USER_KEY, null))) {
+            invalidParams.add(new InvalidProperty(ConanConstants.CONAN_USER_KEY, "Conan user is required"));
+        }
+        if (StringUtil.isEmptyOrSpaces(properties.getOrDefault(ConanConstants.CONAN_CHANNEL_KEY, null))) {
+            invalidParams.add(new InvalidProperty(ConanConstants.CONAN_CHANNEL_KEY, "Conan channel is required"));
+        }
+        return invalidParams;
     }
 
     private void removeEmptyParameters(@NotNull final Map<String, String> properties) {
