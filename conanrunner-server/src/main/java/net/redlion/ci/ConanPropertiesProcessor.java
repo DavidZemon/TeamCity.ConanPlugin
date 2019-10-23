3096dc82ae90d5539a31ac74fd1c0c8229422e9b
David Zemon <david.zemon@redlion.net>
2019-10-23 14:03:16 -0500

    Get rid of any empty parameters (and delete duplicated file)

--- conanrunner-server/src/main/java/net/redlion/ci/ConanPropertiesProcessor.java
+++ conanrunner-server/src/main/java/net/redlion/ci/ConanPropertiesProcessor.java
@@ -2,6 +2,8 @@ package net.redlion.ci;
 
 import jetbrains.buildServer.serverSide.InvalidProperty;
 import jetbrains.buildServer.serverSide.PropertiesProcessor;
+import jetbrains.buildServer.util.StringUtil;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -9,7 +11,12 @@ import java.util.Map;
 
 public class ConanPropertiesProcessor implements PropertiesProcessor {
     @Override
-    public Collection<InvalidProperty> process(Map<String, String> properties) {
+    public Collection<InvalidProperty> process(@NotNull final Map<String, String> properties) {
+        this.removeEmptyParameters(properties);
         return Collections.emptyList();
+    }
+
+    private void removeEmptyParameters(@NotNull final Map<String, String> properties) {
+        properties.entrySet().removeIf(e -> StringUtil.isEmptyOrSpaces(e.getValue()));
     }
 }
